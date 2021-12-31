4b747d287de00bd9dbba7916388abba9b35b7abb
unknown <alextq3036@gmail.com>
2021-12-30 22:57:36 -0500

    Added a play and or stop functionality when the videos are clicked

--- tiktok-frontend/src/Videos.js
+++ tiktok-frontend/src/Videos.js
@@ -1,18 +1,31 @@
-import React from 'react'
-import './Video.css'
+import React, { useRef, useState } from "react";
+import "./Video.css";
 
 function Videos() {
-    return (
-        <div className='video'>
-            <video 
-                className='video__player'
-                src='https://player.vimeo.com/external/463929634.sd.mp4?s=af141dfab13d19b090243b6eb2553c257cd43d75&profile_id=165&oauth2_token_id=57447761'
-                loop
-            >
+  const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState();
 
-            </video>
-        </div>
-    )
+  const handleClick = (e) => {
+    if (!isPlaying) {
+      videoRef.current.play();
+      setIsPlaying(true);
+    } else {
+      videoRef.current.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={handleClick}
+        loop
+        src="https://player.vimeo.com/external/463929634.sd.mp4?s=af141dfab13d19b090243b6eb2553c257cd43d75&profile_id=165&oauth2_token_id=57447761"
+      ></video>
+    </div>
+  );
 }
 
-export default Videos
+export default Videos;
