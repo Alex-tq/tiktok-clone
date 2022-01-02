cf54a14fc5dbdf57ae00231ea0c2ec1d382fd965
unknown <alextq3036@gmail.com>
2022-01-02 01:26:11 -0500

    Created a .env file to store eviromental variables and installed dotenv to give access to .env file to the server

--- tiktok-backend/server.js
+++ tiktok-backend/server.js
@@ -1,15 +1,25 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+//app Config
 const PORT = process.env.PORT || 8001;
 const app = express();
 
+// DB config
+const URI = process.env.MONGODB_URI;
+
+//Middleware
 app.use(express.json());
 
+//Api routes
 app.get("/", (req, res) => {
   res.status(200).send("hello tiktok");
 });
 
+//Server Listener
 app.listen(PORT, () => {
   console.log(`listening on localhost:${PORT}`);
 });
